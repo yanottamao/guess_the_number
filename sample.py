e427191b26e25c97030c035550be9475b98d1118
yanottamao <oktatama4@gmail.com>
2020-11-08 17:15:33 +0700

    Initial multiplayer checker and comment helper placement

--- sample.py
+++ sample.py
@@ -1,12 +1,28 @@
+# library used
 import random
 
+# start messages
 print('Guess the Random Number Game')
 print('----------------------------')
-print('Please Enter Number Beetween 0 - 99')
+
+# initial multi player mode
+mpl = raw_input('Multi Player Mode: ')
+print('mp is ' + mpl)   # mpl input checker
+if mpl.lower() in ['y', 'yes']:
+    print('This is Multi Player Mode')
+    mplc = ('Please Enter Player Count: ')  # player count
+elif mpl.lower() in ['n', 'no']:
+    print('This is Single Player Mode')
+else:
+    print('Please Enter Yes/Y or No/N ')
+
+# standar random 1 to 99
+print('Please Enter Number Beetween 1 - 99')
 rd = random.randint(1, 99)
 gs = 0
 gs = int(input('Enter Your Guess: '))
 
+# simple logic comparator
 while rd != gs:
     if gs < rd:
         print('Hint: Number is Higher Than Your Guess!\n')
@@ -17,5 +33,6 @@ while rd != gs:
     else:
         break
 
+# win messages
 print('Congratulation You Guess It!')
 print('The Mystery Number is ' + str(rd) + '.\n')
