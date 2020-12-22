e08d9e89ab26cb4ca3ae9976110c5934dbe1bf77
yanottamao <oktatama4@gmail.com>
2020-12-23 06:54:03 +0700

    Messing with tkinter

--- sample_gui.py
+++ sample_gui.py
@@ -1,6 +1,10 @@
 import tkinter as tk
 
 window = tk.Tk()
-greeting = tk.Label(text="Hello World")
+greeting = tk.Label(text="Hello World", fg="white",
+                    bg="black", width=10, height=10)
+button = tk.Button(text="Click me!", fg="yellow",
+                   bg="blue", width=25, height=5)
 greeting.pack()
+button.pack()
 window.mainloop()
