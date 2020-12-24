2044821ecc71723c2718f9d0470b7f9de9661a16
yanottamao <oktatama4@gmail.com>
2020-12-24 07:26:39 +0700

    Still messing with tkinter

--- sample_gui.py
+++ sample_gui.py
@@ -5,6 +5,10 @@ greeting = tk.Label(text="Hello World", fg="white",
                     bg="black", width=10, height=10)
 button = tk.Button(text="Click me!", fg="yellow",
                    bg="blue", width=25, height=5)
+label = tk.Label(text="Name")
+entry = tk.Entry(bg="green", fg="yellow", width=50)
 greeting.pack()
 button.pack()
+label.pack()
+entry.pack()
 window.mainloop()
