269207a1b2976fb2b3ffdc515c5fa1386a350657
yanottamao <oktatama4@gmail.com>
2020-12-27 21:53:46 +0700

    Commenting parts

--- sample_gui.py
+++ sample_gui.py
@@ -39,20 +39,20 @@ import random
 # btn_increase = tk.Button(master=window, text="+", command=increase)
 # btn_increase.grid(row=0, column=2, sticky="nsew")
 
-def random_dice():
-    value = int(lbl_value["text"])
-    lbl_value["text"] = f"{random.randint(1, 6)}"
+# def random_dice():
+#     value = int(lbl_value["text"])
+#     lbl_value["text"] = f"{random.randint(1, 6)}"
 
 
 window = tk.Tk()
 
-window.rowconfigure([0, 1], minsize=50, weight=1)
-window.columnconfigure(0, minsize=50, weight=1)
+# window.rowconfigure([0, 1], minsize=50, weight=1)
+# window.columnconfigure(0, minsize=50, weight=1)
 
-btn_roll = tk.Button(master=window, text="ROLL", command=random_dice)
-btn_roll.grid(row=0, column=0, sticky="nsew")
+# btn_roll = tk.Button(master=window, text="ROLL", command=random_dice)
+# btn_roll.grid(row=0, column=0, sticky="nsew")
 
-lbl_value = tk.Label(master=window, text="0")
-lbl_value.grid(row=1, column=0, sticky="nsew")
+# lbl_value = tk.Label(master=window, text="0")
+# lbl_value.grid(row=1, column=0, sticky="nsew")
 
 window.mainloop()
