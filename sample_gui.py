2b779af94b9f8f93125a28088e0e34374a03a7e9
yanottamao <oktatama4@gmail.com>
2020-12-29 17:57:16 +0700

    Still messing with tkinter

--- sample_gui.py
+++ sample_gui.py
@@ -57,21 +57,21 @@ window = tk.Tk()
 
 
 def fah_cel():
-    tempf = float(temp["text"])
-    value = float(lbl_value["text"])
-    lbl_value["text"] = f"{((tempf - 32) * 5/9)}"
+    tempf = temp.get()
+    value = (float(tempf) - 32) * (5/9)
+    lbl_value["text"] = f"{round(value, 2)}"
 
 
 window.rowconfigure([0], minsize=50, weight=1)
 window.columnconfigure([0, 1], minsize=50, weight=1)
 
-temp = tk.Entry(master=window, text="32")
+temp = tk.Entry(master=window, text="")
 temp.grid(row=0, column=0, sticky="nsew")
 
 btn_convert = tk.Button(master=window, text="->", command=fah_cel)
 btn_convert.grid(row=0, column=1, sticky="nsew")
 
-lbl_value = tk.Label(master=window, text="0")
+lbl_value = tk.Label(master=window, text="")
 lbl_value.grid(row=0, column=2)
 
 window.mainloop()
