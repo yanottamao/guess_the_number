ccc21c376884766c9fe030331b07bac6f8a327e6
yanottamao <oktatama4@gmail.com>
2020-12-31 09:32:51 +0700

    Still messing with tkinter

--- sample_gui.py
+++ sample_gui.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter.constants import NSEW
 import random
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+from typing import Text
 
 # window = tk.Tk()
 # greeting = tk.Label(text="Hello World", fg="white",
@@ -44,7 +46,7 @@ import random
 #     lbl_value["text"] = f"{random.randint(1, 6)}"
 
 
-window = tk.Tk()
+# window = tk.Tk()
 
 # window.rowconfigure([0, 1], minsize=50, weight=1)
 # window.columnconfigure(0, minsize=50, weight=1)
@@ -73,5 +75,51 @@ window = tk.Tk()
 
 # lbl_value = tk.Label(master=window, text="")
 # lbl_value.grid(row=0, column=2)
+
+# window.mainloop()
+
+def open_file():
+    """Open a file for editing."""
+    filepath = askopenfilename(
+        filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")]
+    )
+    if not filepath:
+        return
+    txt_edit.delete(1.0, tk.END)
+    with open(filepath, "r") as input_file:
+        text = input_file.read()
+        txt_edit.insert(tk.END, text)
+    window.title(f"Simple Text Editor - {filepath}")
+
+
+def save_file():
+    """Save the current file as a new file."""
+    filepath = asksaveasfilename(
+        defaultextension="txt",
+        filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")]
+    )
+    if not filepath:
+        return
+    with open(filepath, "w") as output_file:
+        text = txt_edit.get(1.0, tk.END)
+        output_file.write(text)
+    window.title(f"Simple Text Editor - {filepath}")
+
+
+window = tk.Tk()
+window.title("Simple Text Editor")
+window.rowconfigure(0, minsize=800, weight=1)
+window.columnconfigure(1, minsize=800, weight=1)
+
+txt_edit = tk.Text(window)
+fr_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
+btn_open = tk.Button(fr_buttons, text="Open", command=open_file)
+btn_save = tk.Button(fr_buttons, text="Save As...", command=save_file)
+
+btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+btn_save.grid(row=1, column=0, sticky="ew", padx=5)
+
+fr_buttons.grid(row=0, column=0, sticky="ns")
+txt_edit.grid(row=0, column=1, sticky="nsew")
 
 window.mainloop()
